3847356ed235efdf05a2b8ad7f3ef8b44b0baeb7
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-06-15 13:25:40 +0200

    Update SpeecheloAPI.ts

--- src/SpeecheloAPI.ts
+++ src/SpeecheloAPI.ts
@@ -69,10 +69,14 @@ export default class SpeecheloAPI {
             return voiceLink
         }
 
+        const html = await page.evaluate(() => document.head.outerHTML + document.head.innerHTML)
+        await browser.close()
+
         throw new Error(
             'Timed out after '
             + (waitingTime * this.afterCreationWaitingStrategy.tries / 1000)
-            + ' seconds'
+            + ' seconds, HTML : \n'
+            + html
         )
     }
 
